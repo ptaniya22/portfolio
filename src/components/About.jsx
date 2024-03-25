781155b194256f3ad77b7f881b0221d0ef830f52
Tiana <tiana.musiyachenko@gmail.com>
2024-03-25 17:09:18 +0500

    added active class to menu

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className='main'>
       <Element name="about" >       
            <div className="main_item"><h2>About me</h2>
-          <p>This is the content of section 2 ***********************************************************************************************************</p>
+          <p>This is the content of section About ***********************************************************************************************************</p>
         </div> 
         {/* <section className='section' style={{ height: 600, backgroundColor: 'lightblue' }}>
           <h1>Обо мне</h1>
@@ -16,16 +16,35 @@ const About = () => {
 
       <Element name="projects" >       
          <div className="main_item"><h2>My projects</h2>
-          <p>This is the content of section 2 *******************************************************************************************</p>
-        </div> 
-        
+          <p>This is the content of section projects *******************************************************************************************</p>
+        </div>         
       </Element>
 
       <Element name="skills" >       
            <div className="main_item"><h2>Skills</h2>
-          <p>This is the content of section 2 *******************************************************************************************</p>
+          <p>This is the content of section Skills *******************************************************************************************</p>
         </div> 
       </Element>
+    
+      {/* <Element name="111" >       
+           <div className="main_item"><h2>contacts</h2>
+          <p>This is the content of section contacts *******************************************************************************************</p>
+        </div> 
+      </Element> */}
+
+
+      <Element name="contacts" >       
+           <div className="main_item"><h2>contacts</h2>
+          <p>This is the content of section contacts *******************************************************************************************</p>
+        </div> 
+      </Element>
+      
+        <Element name="other" >       
+           <div className="main_item"><h2>contacts</h2>
+          <p>This is the content of section contacts *******************************************************************************************</p>
+        </div> 
+      </Element>
+      
     </section>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link, Element } from 'react-scroll';
 
 const list = [
-      { title: 'Обо мне', anchor: 'about' },
-      { title: 'Проекты', anchor: 'projects' },
-      {title:'Навыки',  anchor:'skills'},
+      { title: 'About me', anchor: 'about' },
+      { title: 'Projects', anchor: 'projects' },
+      { title: 'Skills', anchor: 'skills' },
+    
+    // { title: 'aaa', anchor: '111' },
+    { title: 'contacts', anchor: 'contacts' },
+    { title: 'other', anchor: 'other' }
+
+    
 
 ]
 
@@ -12,23 +18,36 @@ const list = [
 
 
 const Navbar = () => {
+
+    // const [click, setclick] = useState(false)
+    // const handleClick = () => setclick(!click)
+
+
+
   return (
       <header className='header'>
-          <h1>Musiyachenko <br />Tatyana</h1>
+          <h1 className='header_title'>Musiyachenko <br />Tatyana</h1>
           <h2>Junior Frontend Engineer</h2>
-          <nav className="nav">
+          <nav className="header_nav">
         
-                <div className="nav__box">
+                {/* <div className="header_nav__box"> */}
                     
-                    <ul className="nav__list">
+                    <ul className="header_nav__list">
                         { list.map(item => (
-                            <li key={item.title}>
-                                <Link to={item.anchor} offset={-96} className="nav__link">{item.title}</Link>
+                            <li  key={item.title}>
+                                <Link to={item.anchor} activeClass="active" offset={-96} spy={true} smooth={true}
+                                    // onClick={handleClick}
+                                    // onClick={handleClick} className={click ? "header_nav__link active" : "header_nav__link"}
+                                    className="header_nav__link">
+                                    <span className="header_nav__link_indicator" ></span>
+                                    <span className="header_nav__link_text">{item.title}</span>
+                                    
+                                </Link>
                             </li>
                         )) }
                     </ul>
             
-                </div>
+                {/* </div> */}
              </nav>
 
         </header>
